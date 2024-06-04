4583dc9937657634ce0b6ec2d23d72bdcc2d823a
Michael James <micrjames921@gmail.com>
2024-06-03 22:30:15 -0700

    added project files

--- ManageFiles.ts
+++ ManageFiles.ts
@@ -1,4 +1,4 @@
-const fs = require("fs");
+import * as fs from "fs";
 
 export class ManageFiles {
    private static _path: string;
@@ -12,5 +12,31 @@ export class ManageFiles {
 
    static get exists(): boolean {
 	  return fs.existsSync(this._path);
+   }
+
+   static get isEmpty(): boolean {
+	  let isEmpty: boolean;
+	  fs.stat(this._path, (err: Error, stats: fs.Stats) => {
+		 if(err) throw err;
+
+		 isEmpty = stats.size == 0;
+	  });
+	  return isEmpty;
+   }
+
+   static create(): string {
+	  if(!this.exists) {
+		 fs.mkdir(this._path, { recursive: false }, (err: Error) => {
+			if(err) throw err;
+		 });
+		 return `file at ${this._path} successfully created.`;
+	  }
+	  return `file at ${this._path} already exists.`; 
+   }
+
+   static read() {
+	  fs.readdir(this._path, {}, (err: Error) => {
+		 if(err) throw err;
+	  });
    }
 }
